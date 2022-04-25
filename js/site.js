dd4170c83f57a58637f4adbf4c7676d952344626
Kevinlevi128 <kevin_colito@hotmail.com>
2022-04-25 17:49:48 -0300

    1er entrega PF

--- js/site.js
+++ js/site.js
@@ -77,7 +77,7 @@ console.log(persona);
 
 
 //ARRAY
-
+/*
 class Producto {
     constructor(nombre, talle, precio) {
         this.nombre  = nombre.toUpperCase();
@@ -103,5 +103,31 @@ productos.push(new Producto("Vestidos", "S", "800"));
 for (const producto of productos)
     producto.sumaIva();
 console.log(productos);
+*/
+
+let = prendas = [];
+class Producto {
+    constructor(nombre, talle, precio) {
+        this.nombre = nombre.toUpperCase();
+        this.talle = talle
+        this.precio = parseFloat(precio);
+        
+    }
+    sumaIva() {
+        this.precio = this.precio * 1.21;
+    }
+}
+var nombre = prompt("ingresa tu nombre");
+        alert("Bienvenido a la tienda " + nombre)
+        alert("Ingrese los datos a continuación para la busqueda de las prendas disponibles y con su precio final.")
+console.log(nombre);
+
+prendas.push(new Producto(prompt("Nombre de prenda: "),
+    prompt("Talle de prenda: "),
+    prompt("Precio: "),
+));
+console.log(prendas);
+for (const producto of prendas)
+    producto.sumaIva();
 
 
